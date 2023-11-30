833468fa8df6de6dfecb866d0eae04cc233d6bbb
AbrarNitk <abrar.nitk@gmail.com>
2023-11-30 23:34:02 +0530

    experiment with linknova

--- dj/linknova/models.py
+++ dj/linknova/models.py
@@ -14,32 +14,48 @@ class DateTimeBase(models.Model):
 
 
 class Topic(DateTimeBase):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, db_index=True, unique=True)
     description = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
 
+    class Meta:
+        db_table = "topic"
+
 
 class Directory(DateTimeBase):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, db_index=True, unique=True)
     title = models.CharField(max_length=255, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
     # user = models.CharField(max_length=255)
     # is_active = models.BooleanField(default=True)
     # public = models.BooleanField(default=True)
     # TODO: Handle roles and permission for different users together
     # TODO: Handle collaboration, for now keeping public and private concept
     # If public so any logged in used can add the content to it.
+    class Meta:
+        db_table = "directory"
 
 
 class Bookmark(DateTimeBase):
     title = models.CharField(max_length=1024)
     url = models.CharField(max_length=4096)
     content = models.TextField(null=True, blank=True)
-    directory = models.ForeignKey(Directory, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
+
     # img_url = models.CharField(max_length=4096, null=True, blank=True)
     # modified = models.BooleanField(default=False)
     # archived = models.BooleanField(default=False)
     # user = models.CharField(max_length=255)
     # tags = ArrayField(models.IntegerField())
+    class Meta:
+        db_table = "bookmark"
+
+
+class BookmarkDirectoryMapping(models.Model):
+    bookmark = models.ForeignKey(Bookmark, on_delete=models.CASCADE)
+    directory = models.ForeignKey(Directory, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "bookmark_directory_map"
--- service/src/api/save.rs
+++ service/src/api/save.rs
@@ -1,3 +1,4 @@
+use sqlx::Row;
 use crate::router::ApiContext;
 
 #[derive(serde::Deserialize)]
@@ -11,5 +12,32 @@ pub async fn save_url(
     axum::extract::State(ctx): axum::extract::State<ApiContext>,
     axum::Json(request): axum::Json<SaveRequest>,
 ) -> String {
+
+    // request: getting the url and categories
+    // get all the categories at runtime and hash-mapped them, with some default value also
+    // insert into url table and get the id
+    //
+    let params = (1..=request.categories.len()).map(|x| format!("${}", x)).collect::<Vec<_>>().join(",");
+    let query_str = format!("select id, name from directory where name in ({})", params);
+    println!("{}", query_str);
+    let mut query = sqlx::query(&query_str);
+    for arg in request.categories.iter() {
+        query = query.bind(arg);
+    }
+    use sqlx::Execute;
+    let sql = query.sql();
+    println!("{}", sql);
+    let rows: Vec<(i64, String)> = query.fetch_all(&ctx.db).await.unwrap().into_iter().map(|row| (row.get(0), row.get(1))).collect();
+
+    // let rows = sqlx::query!("select id, name from directory where name in ('default')")
+    //     .fetch_all(&ctx.db)
+    //     .await.unwrap();
+
+    for (c1, c2) in &rows {
+        println!("{}:{}", c1, c2);
+    }
+
+    println!("{}", rows.len());
+
     "url-saved".to_string()
 }
