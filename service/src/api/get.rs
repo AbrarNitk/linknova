6b78a17ca869b496f2d66dd20c9420e553a9260c
AbrarNitk <abrar.nitk@gmail.com>
2023-12-02 16:55:55 +0530

    get api is done with pagination

--- service/src/api/get.rs
+++ service/src/api/get.rs
@@ -1,28 +1,27 @@
+use sqlx::Row;
 use crate::router::ApiContext;
 
 #[derive(serde::Deserialize, Debug)]
 pub struct GetUrlQuery {
     cat: Option<String>,
-    p_no: Option<usize>,
-    size: Option<usize>,
+    p_no: Option<i64>,
+    size: Option<i64>,
 }
 
 pub async fn get_urls(
     axum::extract::State(ctx): axum::extract::State<ApiContext>,
     axum::extract::Query(query): axum::extract::Query<GetUrlQuery>,
 ) -> axum::response::Response {
-    println!("query: {:?}", query);
-
     // get all the category ids from rh cache
     let cats = query
         .cat
         .unwrap_or("".to_string())
         .split(',')
-        .flat_map(|c| ctx.category_map.get(c))
-        .collect::<Vec<_>>();
-    let p_no = query.p_no.unwrap_or(1);
+        .flat_map(|c| ctx.category_map.get(c).map(|x| *x))
+        .collect::<Vec<i64>>();
+    let p_no = query.p_no.unwrap_or(1);// todo: handle the negative case also
     let size = query.size.unwrap_or(10);
-    match get_urls_(&ctx, &cats, p_no, size).await {
+    match get_urls_(&ctx, cats, p_no, size).await {
         Ok(r) => super::success(axum::http::StatusCode::OK, r),
         Err(e) => {
             println!("{:?}", e);
@@ -42,14 +41,70 @@ pub enum GetUrlsError {
 
 #[derive(serde::Serialize)]
 pub struct GetUrlsResponse {
-    next: Option<usize>,
+    prev: Option<i64>,
+    next: Option<i64>,
+    rows: Vec<UrlRow>,
 }
+
+#[derive(sqlx::FromRow, serde::Serialize)]
+pub struct UrlRow {
+    pub id: i64,
+    pub title: String,
+    pub url: String,
+}
+
 
 pub async fn get_urls_(
     ctx: &ApiContext,
-    categories: &[&i64],
-    p_no: usize,
-    size: usize,
+    categories: Vec<i64>,
+    p_no: i64,
+    size: i64,
 ) -> Result<GetUrlsResponse, GetUrlsError> {
-    Ok(GetUrlsResponse {})
+    let mut query = "select DISTINCT ON (bookmark.id) bookmark.id, bookmark.title, bookmark.url from bookmark join bookmark_directory_map on bookmark.id=bookmark_directory_map.bookmark_id ".to_string();
+    if !categories.is_empty() {
+        query.push_str(" where bookmark_directory_map.directory_id = ANY($1) and is_active = true ");
+        query.push_str(" order by bookmark.id, created_on DESC OFFSET $2 LIMIT $3");
+    } else {
+        query.push_str(" where is_active = true ");
+        query.push_str(" order by bookmark.id, created_on DESC OFFSET $1 LIMIT $2");
+    }
+
+    let mut db_query = sqlx::query(query.as_str());
+    if !categories.is_empty() {
+        db_query = db_query.bind(&categories[..]);
+    }
+
+    let offset: i64 = if p_no <= 1 { 0 } else { ((p_no - 1) * size) - 1 };
+
+    let rows: Vec<UrlRow> = db_query
+        .bind(offset)
+        .bind(size + 1)
+        .fetch_all(&ctx.db).await?
+        .into_iter()
+        .map(|r| UrlRow {
+            id: r.get(0),
+            title: r.get(1),
+            url: r.get(2),
+        }).collect();
+
+    let mut next = None;
+    if rows.len() > size as usize {
+        next = Some(p_no + 1);
+    }
+
+    let mut prev = None;
+    if p_no > 1 {
+        prev = Some(p_no - 1);
+    }
+
+    Ok(GetUrlsResponse {
+        prev,
+        next,
+        rows: rows.into_iter().take(size as usize).collect(),
+    })
 }
+
+//    let q = "select * from bookmark join bookmark_directory_map on bookmark.id=bookmark_directory_map.bookmark_id";
+//    let q = r#"select bookmark.* from bookmark join bookmark_directory_map on bookmark.id=bookmark_directory_map.bookmark_id where bookmark_directory_map.directory_id in (1, 2, 3, 4, 5) and is_active = true order by created_on desc OFFSET 2 LIMIT 3"#;
+
+
