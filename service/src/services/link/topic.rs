e03e7e6575a13e8a06f064582289163ab74ade70
AbrarNitk <abrar.nitk@gmail.com>
2025-07-23 12:57:37 +0530

    topic service

--- service/src/services/link/topic.rs
+++ service/src/services/link/topic.rs
@@ -1,1 +1,29 @@
+pub mod types;
 
+use crate::controller::link;
+use crate::ctx::Ctx;
+
+#[tracing::instrument(name = "service::topic-create", skip_all)]
+pub fn create(ctx: &Ctx, req: link::topic::CreateRequest) -> Result<(), types::TopicError> {
+    Ok(())
+}
+
+#[tracing::instrument(name = "service::topic-get", skip_all)]
+pub fn get(ctx: &Ctx, topic_name: &str) -> Result<(), types::TopicError> {
+    Ok(())
+}
+
+#[tracing::instrument(name = "service::topic-list", skip_all)]
+pub fn list(ctx: &Ctx) -> Result<(), types::TopicError> {
+    Ok(())
+}
+
+#[tracing::instrument(name = "service::topic-update", skip_all)]
+pub fn update(ctx: &Ctx, topic_name: &str) -> Result<(), types::TopicError> {
+    Ok(())
+}
+
+#[tracing::instrument(name = "service::topic-delete", skip_all)]
+pub fn delete(ctx: &Ctx, topic_name: &str) -> Result<(), types::TopicError> {
+    Ok(())
+}
