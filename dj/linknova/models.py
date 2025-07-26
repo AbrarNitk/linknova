b71dc26541735ea8fe957b6307ca320f294a134d
AbrarNitk <abrar.nitk@gmail.com>
2025-07-26 21:18:39 +0530

    bookmark models

--- dj/linknova/models.py
+++ dj/linknova/models.py
@@ -48,7 +48,7 @@ class Category(DateTimeBase):
 
 
 # a topic can have multiple categories
-# a category can be present in the mulitple topics
+# a category can be present in the multiple topics
 class TopicCategoryMapping(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -58,28 +58,28 @@ class TopicCategoryMapping(models.Model):
         unique_together = [["category", "topic"]]
 
 
-# class Bookmark(DateTimeBase):
-#     title = models.CharField(max_length=1024, blank=True, null=True)
-#     url = models.CharField(max_length=4096)
-#     content = models.TextField(null=True, blank=True)
-#     is_active = models.BooleanField(default=True)
-#     reference = models.CharField(max_length=1024, null=True, blank=True)
-#     categories = models.JSONField(default=dict)
+class Bookmark(DateTimeBase):
+    url = models.CharField(max_length=4096)
+    user_id = models.CharField(max_length=255, db_index=True)
+    title = models.CharField(max_length=1024, blank=True, null=True)
+    content = models.TextField(null=True, blank=True)
+    referrer = models.CharField(max_length=1024, blank=True, null=True)
+    # UN: Unread
+    # RD: Read
+    # AR: Archived
+    status = models.CharField(max_length=2)
+
+    class Meta:
+        db_table = "linknova_bookmark"
+
+
 #
-#     # img_url = models.CharField(max_length=4096, null=True, blank=True)
-#     # modified = models.BooleanField(default=False)
-#     # archived = models.BooleanField(default=False)
-#     # user = models.CharField(max_length=255)
-#     # tags = ArrayField(models.IntegerField())
-#     class Meta:
-#         db_table = "linknova_bookmark"
 #
-#
-# # a bookmark can belongs to multiple categories
-# class BookmarkCategoryMapping(models.Model):
-#     bookmark = models.ForeignKey(Bookmark, on_delete=models.CASCADE)
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-#
-#     class Meta:
-#         db_table = "linknova_bookmark_category_map"
-#         unique_together = [["category", "bookmark"]]
+# a bookmark can belongs to multiple categories
+class BookmarkCategoryMapping(models.Model):
+    bookmark = models.ForeignKey(Bookmark, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "linknova_bookmark_category_map"
+        unique_together = [["category", "bookmark"]]
